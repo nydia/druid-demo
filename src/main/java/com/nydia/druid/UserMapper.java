2e50fc8b9ad9a80d3cf3f8aa37ab1198c67dd8cf
nydia <nydia_lvhq@sina.cn>
2021-11-08 18:47:59 +0800

    20211108

--- src/main/java/com/nydia/druid/UserMapper.java
+++ src/main/java/com/nydia/druid/UserMapper.java
@@ -6,8 +6,8 @@ import org.apache.ibatis.annotations.Select;
 /**
  * @Description TODO
  * @Date 2021/11/8 12:47
- * @Created by <a href="mailto:nydia_lhq@hotmail.com">lvhuaqiang</a>
- */
+        * @Created by <a href="mailto:nydia_lhq@hotmail.com">lvhuaqiang</a>
+        */
 @Mapper
 public interface UserMapper {
     @Select(value = "select id,name,age from user where id = #{id}")
